2beb4d8aa7a2ade054ef0a6bb1f3e9c366c703aa
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-27 16:29:45 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// deploy repo is closed no longer available for any type of deployment 👀
+
+// Deploy From New Repository ✅ 
+
+// KHAN-MD version 5.0 ❤️
+
+// Thanks For Chosing KHAN MD 🔥
+
+// Made with ♥️ By JawadTech 
+
+// Powered By JawadTechX KHAN-MD 2025 🚀
